50dd93b03b4dfb370246483a8ef25414b9c7ed32
lanbery <lanbery@163.com>
2024-09-06 02:18:07 +0800

    feat: wrap aes util

--- src/core/utils/crypto/aes-utils.spec.ts
+++ src/core/utils/crypto/aes-utils.spec.ts
@@ -2,7 +2,8 @@ import { AESCipher, AES_KEY_LENGTH, genRandomAppKey } from './aes-utils';
 
 describe('AES-Utils', () => {
   const text = '18811264567';
-  const appkey = '';
+  const appkey = 'iBOrlZ6A5Mpy2V2lYqo1CD65Ux5KPCWb';
+  const encodeBaseText = 'iOSngPTREicABv2eQBR7KA==';
   describe('get random key', () => {
     test('Test random key length', () => {
       const key = genRandomAppKey();
@@ -27,9 +28,18 @@ describe('AES-Utils', () => {
       const enData = aes.encode(text);
       const base64 = AESCipher.hexToBase64(enData);
 
+      // globalThis.console.log(aes.opts, base64);
+
       const decrypedText = aes.decode(AESCipher.fromBase64ToHex(base64));
 
       expect(decrypedText).toEqual(text);
+    });
+
+    test(`encode ${text} will to be ${encodeBaseText}`, () => {
+      const aes = new AESCipher(appkey);
+      const encrypted = AESCipher.hexToBase64(aes.encode(text));
+
+      expect(encrypted).toEqual(encodeBaseText);
     });
   });
 });
